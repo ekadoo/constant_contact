6fb2adb5dec2984f683a9c9c643559a3c380c171
Tiago Paulino <tspaulino@gmail.com>
2013-04-06 02:36:28 -0300

    Adding bulk contacts to a list

--- lib/constant_contact/list.rb
+++ lib/constant_contact/list.rb
@@ -1,8 +1,9 @@
 module ConstantContact
   class List < Base
 
-    # @@column_names = [:contact_count, :display_on_signup, :members, :name, :opt_in_default, :short_name, :sort_order]    
-    
+    # @@column_names = [:contact_count, :display_on_signup, :members, :name, :opt_in_default, :short_name, :sort_order]
+    attr_reader :new_members
+
     def to_xml
       xml = Builder::XmlMarkup.new
       xml.tag!("ContactList", :xmlns => "http://ws.constantcontact.com/ns/1.0/") do
@@ -14,5 +15,25 @@ module ConstantContact
       lists = self.find :all
       lists.find{|list| list.Name == name}
     end
+
+    def add_member(member)
+      @new_members = [] unless defined?(@members)
+      @new_members << member
+    end
+
+    def subscribe_members!
+      @new_members.each do |member|
+        existing_member = Contact.find_by_email(member.email)
+        if existing_member.present?
+          member = existing_member
+          member.contact_lists.nil ? member.contact_lists=[self.id] : member.contact_lists << self.class.parse_id(self.id)
+        else
+          member.contact_lists = [self.class.parse_id(self.id)]
+        end
+        puts "trying to save #{member.encode}"
+        member.save
+      end
+    end
+
   end
 end
